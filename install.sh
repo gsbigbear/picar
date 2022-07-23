6f8c1d35d629118c3d00f3da787d059aef779723
gsbigbear <109757157+gsbigbear@users.noreply.github.com>
2022-07-23 13:28:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ git clone https://github.com/autorope/donkeycar
 cd donkeycar
 git fetch --all --tags
 git checkout 4.3.6-17-g5143794
-pip3 install -e .[pi]
-pip3 install https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.2.0/tensorflow-2.2.0-cp37-none-linux_armv7l.whl
+pip3 install -e .[pi] 
+pip3 install https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.2.0/tensorflow-2.2.0-cp37-none-linux_armv7l.whl 
 cd /home/pi
 git clone https://github.com/gsbigbear/picar.git
