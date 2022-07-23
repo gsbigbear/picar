c884b562beb2c1590bf3406a74c939c013e98bba
gsbigbear <109757157+gsbigbear@users.noreply.github.com>
2022-07-23 12:49:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 apt-get update --allow-releaseinfo-change
 apt-get upgrade -y
 apt-get install -y build-essential python3 python3-dev python3-pip python3-virtualenv python3-numpy python3-picamera python3-pandas python3-rpi.gpio i2c-tools avahi-utils joystick libopenjp2-7-dev libtiff5-dev gfortran libatlas-base-dev libopenblas-dev libhdf5-serial-dev libgeos-dev git ntp
-apt-get -y install libilmbase-dev libopenexr-dev libgstreamer1.0-dev libjasper-dev libwebp-dev libatlas-base-dev libavcodec-dev libavformat-dev libswscale-dev libqtgui4 libqt4-test
+apt-get install -y libilmbase-dev libopenexr-dev libgstreamer1.0-dev libjasper-dev libwebp-dev libatlas-base-dev libavcodec-dev libavformat-dev libswscale-dev libqtgui4 libqt4-test
 apt-get install -y tmux mplayer
 cd /home/pi
 python3 -m virtualenv -p python3 env --system-site-packages
